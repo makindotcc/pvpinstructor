75da968a1dd6a16c0d1c9e602fb54d1bfb5a17d3
makindotcc <9150636+makindotcc@users.noreply.github.com>
2021-08-10 16:44:54 +0200

    rice message color

--- src/main/java/cc/makin/pvpinstructor/Instructor.kt
+++ src/main/java/cc/makin/pvpinstructor/Instructor.kt
@@ -7,6 +7,7 @@ import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEvent
+import java.awt.Color
 
 class Instructor(
     private val settingStorage: PvpInstructorSettingStorage,
@@ -15,23 +16,33 @@ class Instructor(
     fun handleAttack(event: PlayerInteractEvent) {
         when (event.action) {
             Action.LEFT_CLICK_AIR,
-            Action.LEFT_CLICK_BLOCK -> handleAttack(event.player)
+            Action.LEFT_CLICK_BLOCK,
+            -> handleAttack(event.player)
             else -> {}
         }
     }
 
     private fun handleAttack(player: Player) = run {
         if (this.settingStorage.hasEnabledInstructor(player.uniqueId)) {
-            val message = getMessage(player.attackCooldown)
-            player.sendTitle(" ", message, 0, 5, 5)
+            val cd = player.attackCooldown
+            val message = getMessage(cd)
+            val coloredMessage = successRateColor(cd).toString() + message
+            player.sendTitle(" ", coloredMessage, 0, 5, 5)
         }
     }
 
     private fun getMessage(cooldown: Float) = when (cooldown) {
-        in 0.0f..0.4f -> "${ChatColor.DARK_RED}fatalnie"
-        in 0.4f..0.6f -> "${ChatColor.RED}słabo"
-        in 0.6f..0.8f -> "${ChatColor.YELLOW}średnio"
-        in 0.8f..0.99f -> "${ChatColor.GREEN}prawie dobrze"
-        else -> "${ChatColor.GREEN}idealnie"
+        in 0.0f..0.4f -> "fatalnie"
+        in 0.4f..0.6f -> "słabo"
+        in 0.6f..0.8f -> "średnio"
+        in 0.8f..0.99f -> "prawie dobrze"
+        else -> "idealnie"
+    }
+
+    private fun successRateColor(cooldownPercent: Float): ChatColor = run {
+        val hue = cooldownPercent * 0.3f
+        val saturation = 0.9f
+        val brightness = 1.0f
+        ChatColor.of(Color.getHSBColor(hue, saturation, brightness))
     }
 }
